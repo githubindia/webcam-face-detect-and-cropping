f0fae1fca80f67b572a8235038fa304d3de357a9
pourabk-hexaware <shubham08gupta08@gmail.com>
2018-07-10 21:24:43 +0530

    nose and mouth added

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -8,9 +8,11 @@ from PIL import Image
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+mouth_cascade = cv2.CascadeClassifier('mouth.xml')
+nose_cascade = cv2.CascadeClassifier('nariz.xml')
 log.basicConfig(filename='webcam.log',level=log.INFO)
 
-video_capture = cv2.VideoCapture('sample_video.mov')
+video_capture = cv2.VideoCapture('Sample_video.mov')
 anterior = 0
 
 while True:
@@ -33,17 +35,23 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         # sub_face = frame[y:y+h, x:x+w]
         # FaceFileName = "unknownfaces/face_" + str(y) + ".jpg"
         # cv2.imwrite(FaceFileName, sub_face)
         roi_gray = gray[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        mouth = mouth_cascade.detectMultiScale(roi_gray)
+        nose = nose_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
-            # cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            sub_face = frame[y:y+h, x:x+w]
-            FaceFileName = "unknownfaces/face_" + str(y) + ".jpg"
-            cv2.imwrite(FaceFileName, sub_face)
+            cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(22,255,0),2)
+            for (mx,my,mw,mh) in mouth:
+                cv2.rectangle(frame,(mx,my),(mx+mw,my+mh),(122,255,0),2)
+                for (nx,ny,nw,nh) in nose:
+                    cv2.rectangle(frame,(nx,ny),(nx+nw,ny+nh),(0,255,111),2)
+                    sub_face = frame[y:y+h, x:x+w]
+                    FaceFileName = "unknownfaces/face_" + str(y) + ".jpg"
+                    cv2.imwrite(FaceFileName, sub_face)
 
     if anterior != len(faces):
         anterior = len(faces)
